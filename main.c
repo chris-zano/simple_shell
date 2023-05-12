fea4c073a8f6a03b37cfb70f0e16b483355bb73e
AropetJ <aropetjoel32@gmail.com>
2023-05-12 15:02:02 +0200

    main.c betty check

--- main.c
+++ main.c
@@ -1,7 +1,12 @@
 #include "shell.h"
+/**
+ * main - entry point of the program.
+ *
+ * Return: 0 Always.
+ */
 
-int main()
+int main(void)
 {
-    loop();
-    return 0;
+	loop();
+	return (0);
 }

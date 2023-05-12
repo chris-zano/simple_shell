cae5cb00f9e2f85462128efded7945f1b62f8c2f
AropetJ <aropetjoel32@gmail.com>
2023-05-12 14:53:04 +0200

    exit.c passed betty check

--- exit.c
+++ exit.c
@@ -1,6 +1,13 @@
 #include "shell.h"
 
+/**
+ * dash_exit - to function that exits the shell.
+ *
+ * @args: arguments passed by the user.
+ *
+ * Return: 0 Always.
+ */
 int dash_exit(char **args)
 {
-    return 0;
+	return (0);
 }

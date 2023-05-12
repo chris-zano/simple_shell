b0038a79eb7f494d4671f0aa226bc55470b3b587
AropetJ <aropetjoel32@gmail.com>
2023-05-12 14:59:39 +0200

    loop.c betty check

--- loop.c
+++ loop.c
@@ -1,29 +1,32 @@
 #include "shell.h"
+/**
+ * loop - the main loop that executes the shell.
+ *
+ * Return: void.
+ */
 
-void loop()
+void loop(void)
 {
-    char *line;
-    char **args;
-    char *command;
-    int status = 1;
+	char *line;
+	char **args;
+	char *command;
+	int status = 1;
 
-    do
-    {
-        printf("~$ ");
-        line = read_line();
-        args = split_line(line);
-	if (strcmp(args[0], "exit") == 0)
-        {
-                dash_exit(args);
-		return;
-        }
-	else
-	{
-		command = search_path(args);
-		dash_execute(command, args);
-	}
-        free(line);
-        free(args);
-    }
-    while (status);
+	do {
+		printf("~$ ");
+		line = read_line();
+		args = split_line(line);
+		if (strcmp(args[0], "exit") == 0)
+		{
+			dash_exit(args);
+			return;
+		}
+		else
+		{
+			command = search_path(args);
+			dash_execute(command, args);
+		}
+		free(line);
+		free(args);
+	} while (status);
 }

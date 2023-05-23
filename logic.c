0f264ea0faedb9251f29cf3aceca86d640a25e0c
Christian Solomon <cncs.chris@gmail.com>
2023-05-23 11:56:48 +0000

    fixed segfault bug in command separators

--- logic.c
+++ logic.c
@@ -2,19 +2,26 @@
 
 /**
  * dash_logic - handles the && and || logical operators
- * 
+ *
  * @args: command line arguments
-*/
+ */
 void dash_logic(char **args)
 {
 	int status;
 	pid_t cpid;
+	char *builtin_args[] = {"exit", "setenv", "unsetenv", "cd", "alias", NULL};
+	char *command;
 
 	cpid = fork();
 
 	if (cpid == 0)
-	{        
-		execvp(args[0], args);
+	{
+		// execvp(args[0], args);
+		if (is_builtin(args[0], args, builtin_args) == 0)
+		{
+			command = search_path(args);
+			dash_execute(command, args);
+		}
 		perror("dash: Error executing command");
 		exit(EXIT_FAILURE);
 	}
@@ -28,6 +35,11 @@ void dash_logic(char **args)
 		else if (status != 0 && args[1] != NULL && _strcmp(args[1], "||") == 0)
 		{
 			dash_logic(&args[2]);
+		}
+		else if (status < 0)
+		{
+			perror("dash: Error executing command");
+			exit(EXIT_FAILURE);
 		}
 	}
 	else
